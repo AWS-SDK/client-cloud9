553062304e242d01af29da7348e13957f4e5b05f
suryavaddiraju <112264612+suryavaddiraju@users.noreply.github.com>
2025-07-14 14:33:11 +0000

    chore: update to 3.844.0

--- examples/main.mjs
+++ examples/main.mjs
@@ -1,3 +1,3 @@
-import {ClientCommand } from "@aws-sdk/{repoName}";
+import {ClientCommand } from "@aws-sdk/client-cloud9";
 
 // Refer "https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/" for documentation of this command
